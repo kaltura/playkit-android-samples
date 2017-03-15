673aa109179bd1dc1cc00643246633d3f3925924
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-03-16 01:34:13 +0200

    first playback for EventsRegistration

---
+++ EventsRegistration/app/src/main/java/com/kaltura/playkit/samples/eventsregistration/MainActivity.java
@@ -0,0 +1,180 @@
+package com.kaltura.playkit.samples.eventsregistration;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.kaltura.playkit.PKMediaConfig;
+import com.kaltura.playkit.PKMediaEntry;
+import com.kaltura.playkit.PKMediaFormat;
+import com.kaltura.playkit.PKMediaSource;
+import com.kaltura.playkit.PKPluginConfigs;
+import com.kaltura.playkit.PlayKitManager;
+import com.kaltura.playkit.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int START_POSITION = 0;
+
+    //The url of the source to play
+    private static final String SOURCE_URL = "https://cdnapisec.kaltura.com/p/2215841/sp/221584100/playManifest/entryId/1_w9zx2eti/protocol/https/format/applehttp/falvorIds/1_1obpcggb,1_yyuvftfz,1_1xdbzoa6,1_k16ccgto,1_djdf6bk8/a.m3u8";
+
+    private static final String ENTRY_ID = "entry_id";
+    private static final String MEDIA_SOURCE_ID = "source_id";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMenu();
+            }
+        });
+    }
+
+    void showMenu() {
+        final Context context = this;
+        new AlertDialog.Builder(context)
+                .setItems(new String[]{ "Play", "Pause"}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Selected " + which, Toast.LENGTH_SHORT).show();
+                        if (which == 0) {
+                            playDemoVideo();
+                        }
+                    }
+                })
+                .show();
+    }
+
+    private void playDemoVideo() {
+        //First. Create PKMediaConfig object.
+        PKMediaConfig mediaConfig = new PKMediaConfig()
+                // You can configure the start position for it.
+                // by default it will be 0.
+                // If start position is grater then duration of the source it will be reset to 0.
+                .setStartPosition(START_POSITION);
+
+        //Second. Create PKMediaEntry object.
+        PKMediaEntry mediaEntry = createMediaEntry();
+
+        //Add it to the mediaConfig.
+        mediaConfig.setMediaEntry(mediaEntry);
+
+        //Create plugin config. In this sample we are not discussing plugins,
+        //so we will use a default instance of this object.
+        PKPluginConfigs pluginConfig = new PKPluginConfigs();
+
+        //Create instance of the player.
+        Player player = PlayKitManager.loadPlayer(pluginConfig, this);
+
+        //Get the layout, where the player view will be placed.
+        LinearLayout layout = (LinearLayout) findViewById(R.id.player_root);
+        //Add player view to the layout.
+        layout.addView(player.getView());
+
+        //Prepare player with media configuration.
+        player.prepare(mediaConfig);
+
+        //Start playback.
+        player.play();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Create {@link PKMediaEntry} with minimum necessary data.
+     * @return - the {@link PKMediaEntry} object.
+     */
+    private PKMediaEntry createMediaEntry() {
+        //Create media entry.
+        PKMediaEntry mediaEntry = new PKMediaEntry();
+
+        //Set id for the entry.
+        mediaEntry.setId(ENTRY_ID);
+
+        //Set media entry type. It could be Live,Vod or Unknown.
+        //For now we will use Unknown.
+        mediaEntry.setMediaType(PKMediaEntry.MediaEntryType.Unknown);
+
+        //Create list that contains at least 1 media source.
+        //Each media entry can contain a couple of different media sources.
+        //All of them represent the same content, the difference is in it format.
+        //For example same entry can contain PKMediaSource with dash and another
+        // PKMediaSource can be with hls. The player will decide by itself which source is
+        // preferred for playback.
+        List<PKMediaSource> mediaSources = createMediaSources();
+
+        //Set media sources to the entry.
+        mediaEntry.setSources(mediaSources);
+
+        return mediaEntry;
+    }
+
+    /**
+     * Create list of {@link PKMediaSource}.
+     * @return - the list of sources.
+     */
+    private List<PKMediaSource> createMediaSources() {
+        //Init list which will hold the PKMediaSources.
+        List<PKMediaSource> mediaSources = new ArrayList<>();
+
+        //Create new PKMediaSource instance.
+        PKMediaSource mediaSource = new PKMediaSource();
+
+        //Set the id.
+        mediaSource.setId(MEDIA_SOURCE_ID);
+
+        //Set the content url. In our case it will be link to hls source(.m3u8).
+        mediaSource.setUrl(SOURCE_URL);
+
+        //Set the format of the source. In our case it will be hls.
+        mediaSource.setMediaFormat(PKMediaFormat.hls);
+
+        //Add media source to the list.
+        mediaSources.add(mediaSource);
+
+        return mediaSources;
+    }
+
+}
+

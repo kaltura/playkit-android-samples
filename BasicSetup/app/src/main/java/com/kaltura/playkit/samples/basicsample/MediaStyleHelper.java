18372517d5fd8318ec28f8803f59e0bf943adc6a
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-06-09 02:08:18 +0300

    add mediaSession sample into basicSample

---
+++ BasicSetup/app/src/main/java/com/kaltura/playkit/samples/basicsample/MediaStyleHelper.java
@@ -0,0 +1,33 @@
+package com.kaltura.playkit.samples.basicsample;
+
+import android.content.Context;
+import android.support.v4.media.MediaDescriptionCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v7.app.NotificationCompat;
+
+
+public class MediaStyleHelper {
+
+    public static NotificationCompat.Builder from(
+            Context context, MediaSessionCompat mediaSession) {
+        MediaControllerCompat controller = mediaSession.getController();
+        MediaMetadataCompat mediaMetadata = controller.getMetadata();
+        MediaDescriptionCompat description = mediaMetadata.getDescription();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder
+                .setContentTitle(description.getTitle())
+                .setContentText(description.getSubtitle())
+                .setSubText(description.getDescription())
+                .setLargeIcon(description.getIconBitmap())
+                .setContentIntent(controller.getSessionActivity())
+                .setDeleteIntent(
+                        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_STOP))
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+        return builder;
+    }
+}

9ad534176100a8b045c9f2cc751b56bbb3af88ad
CI Slave <cislave@FrontEnd-CI-Mac.local>
2017-11-16 18:45:36 +0200

    fix url

--- ChangeMedia/app/src/main/java/com/kaltura/playkit/samples/changemedia/MainActivity.java
+++ ChangeMedia/app/src/main/java/com/kaltura/playkit/samples/changemedia/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     //The url of the first source to play
-    private static final String FIRST_SOURCE_URL = "https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8";
+    private static final String FIRST_SOURCE_URL = "http://cdnapi.kaltura.com/p/243342/sp/24334200/playManifest/entryId/0_uka1msg4/flavorIds/1_vqhfu6uy,1_80sohj7p/format/applehttp/protocol/http/a.m3u8";
     //The url of the second source to play
     private static final String SECOND_SOURCE_URL = "https://cdnapisec.kaltura.com/p/2215841/sp/221584100/playManifest/entryId/1_w9zx2eti/protocol/https/format/url/falvorIds/1_1obpcggb,1_yyuvftfz,1_1xdbzoa6,1_k16ccgto,1_djdf6bk8/a.mp4";
 

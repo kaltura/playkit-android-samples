89f0d242bd2fd0e967e81901291c8428f82885ba
GouravSna <38206744+GouravSna@users.noreply.github.com>
2018-11-15 20:57:26 +0530

    - Added Subtitles styling Spinner (#12)
    * - Added Subtitles styling Spinner
    
    * - Fixed Spinner visibility
    
    * reorder repos
    
    * make subtitles demo running
    
    * add listener to subtitles changed
    
    * - Refactored subtile styles setters/getters
    
    * - Refactored variable names for subtitle style spinners
    
    * - Commented local repos from settings.gradle

---
+++ FullDemo/app/src/main/java/com/kaltura/playkit/samples/fulldemo/LeakTest.java
@@ -0,0 +1,18 @@
+package com.kaltura.playkit.samples.fulldemo;
+
+import android.app.Application;
+
+
+//public class LeakTest extends Application {
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        //if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//          //  return;
+//        //}
+//        //LeakCanary.install(this);
+//    }
+//}

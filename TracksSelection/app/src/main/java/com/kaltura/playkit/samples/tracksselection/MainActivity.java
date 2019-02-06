1dda8ac0152d73d447f8068ed8b838cdf774c99b
GouravSna <38206744+GouravSna@users.noreply.github.com>
2019-02-06 17:53:49 +0530

    FEM-2237 Sample configuration for track selection to select multiple tracks so it works as ABR (#18)
    * - Added ABR selection api for min/max and estimation to TrackSelection Sample
    
    * - Update with new methods
    
    * Update MainActivity.java

--- TracksSelection/app/src/main/java/com/kaltura/playkit/samples/tracksselection/MainActivity.java
+++ TracksSelection/app/src/main/java/com/kaltura/playkit/samples/tracksselection/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
-import com.kaltura.playkit.PKEvent;
 import com.kaltura.playkit.PKMediaConfig;
 import com.kaltura.playkit.PKMediaEntry;
 import com.kaltura.playkit.PKMediaFormat;
@@ -21,7 +20,7 @@ import com.kaltura.playkit.PKMediaSource;
 import com.kaltura.playkit.PlayKitManager;
 import com.kaltura.playkit.Player;
 import com.kaltura.playkit.PlayerEvent;
-import com.kaltura.playkit.ads.AdController;
+import com.kaltura.playkit.player.ABRSettings;
 import com.kaltura.playkit.player.AudioTrack;
 import com.kaltura.playkit.player.PKTracks;
 import com.kaltura.playkit.player.SubtitleStyleSettings;
@@ -84,6 +83,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ////player.getSettings().setPreferredTextTrack(new PKTrackConfig().setPreferredMode(PKTrackConfig.Mode.AUTO)); // select the track by locale if does not exist manifest default
         //player.getSettings().setPreferredTextTrack(new PKTrackConfig().setPreferredMode(PKTrackConfig.Mode.EXPLICIT).setTrackLanguage("rus")); // select specific track lang if not exist select manifest default
         ////player.getSettings().setPreferredAudioTrack(new PKTrackConfig().setPreferredMode(PKTrackConfig.Mode.AUTO));
+
+        // --->  Min-Max video bitrate
+        // --->  Sets the initial bitrate estimate in bits per second that should be assumed when a bandwidth estimate is unavailable.
+        player.getSettings().setABRSettings(new ABRSettings().setMinVideoBitrate(250000).setMaxVideoBitrate(3000000).setInitialBitrateEstimate(100000));
 
         player.getSettings().setSubtitleStyle(getDefaultPositionDefault());
 
@@ -263,6 +266,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         player.addListener(this, PlayerEvent.subtitlesStyleChanged, event -> {
             Log.d(TAG, "Event SubtitlesStyleChanged " + event.styleName);
+
+        });
+
+        player.addListener(this, PlayerEvent.error, event -> {
+            PlayerEvent.Error playerError = event;
+            if (playerError != null && playerError.error != null) {
+                Log.d(TAG, "PlayerEvent.Error event  position = " + playerError.error.errorType + " errorMessage = " + playerError.error.message);
+            }
 
         });
     }

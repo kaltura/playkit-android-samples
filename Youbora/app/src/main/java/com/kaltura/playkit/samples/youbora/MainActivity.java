3c705eebe2a6628413b9913e33fc0dd308da4299
Gourav Saxena <gourav.saxena@kaltura.com>
2019-07-17 16:40:37 +0530

    - Youbora bundle keys changes

--- Youbora/app/src/main/java/com/kaltura/playkit/samples/youbora/MainActivity.java
+++ Youbora/app/src/main/java/com/kaltura/playkit/samples/youbora/MainActivity.java
@@ -22,7 +22,34 @@ import com.kaltura.playkit.plugins.youbora.YouboraPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_CAST;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_DIRECTOR;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_OWNER;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_PARENTAL;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_QUALITY;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_RATING;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_CONTENT_METADATA_YEAR;
+import static com.kaltura.playkit.plugins.youbora.pluginconfig.YouboraConfig.KEY_HOUSEHOLD_ID;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_ACCOUNT_CODE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_AD_CAMPAIGN;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_CHANNEL;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_ENCODING_AUDIO_CODEC;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_GENRE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_METADATA;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_PRICE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_TITLE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_TRANSACTION_CODE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CONTENT_TYPE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CUSTOM_DIMENSION_1;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_CUSTOM_DIMENSION_2;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_DEVICE_BRAND;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_DEVICE_CODE;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_DEVICE_MODEL;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_DEVICE_OS_NAME;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_DEVICE_OS_VERSION;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_DEVICE_TYPE;
 import static com.npaw.youbora.lib6.plugin.Options.KEY_ENABLED;
+import static com.npaw.youbora.lib6.plugin.Options.KEY_USERNAME;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     //The url of the source to play
-    private static final String SOURCE_URL = "https://cdnapisec.kaltura.com/p/1982551/sp/198255100/playManifest/protocol/https/entryId/0_if268bdo/format/applehttp/tags/iphonenew/f/a.m3u8";
+    private static final String SOURCE_URL = "https://cdnapisec.kaltura.com/p/2215841/playManifest/entryId/1_w9zx2eti/format/mpegdash/protocol/https/a.mpd";
 
     //The id of the entry.
     private static final String ENTRY_ID = "entry_id";
@@ -49,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_PARAM_2 = "";
     public static final String GENRE = "your_genre";
     public static final String TYPE = "your_type";
-    public static final String TRANSACTION_TYPE = "your_trasnsaction_type";
+    public static final String TRANSACTION_TYPE = "your_transaction_type";
     public static final String YEAR = "your_year";
     public static final String CAST = "your_cast";
     public static final String DIRECTOR = "your_director";
@@ -58,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String PRICE = "your_price";
     public static final String RATING = "your_rating";
     public static final String AUDIO_TYPE = "your_audio_type";
-    public static final String AUDIO_CHANNELS = "your_audoi_channels";
+    public static final String AUDIO_CHANNELS = "your_audio_channels";
     public static final String DEVICE = "your_device";
     public static final String QUALITY = "your_quality";
 
@@ -75,16 +102,11 @@ public class MainActivity extends AppCompatActivity {
         //Initialize media config object.
         createMediaConfig();
 
-        //Important!!! First you need to register your plugin.
-        PlayKitManager.registerPlugins(this, YouboraPlugin.factory);
-
         //Initialize PKPluginConfigs object with Youbora.
-     //   PKPluginConfigs pluginConfigs = new PKPluginConfigs();
-        //Set plugin entry to the plugin configs.
-
+        PKPluginConfigs pluginConfigs = getYouboraBundle();
 
         //Create instance of the player with specified pluginConfigs.
-        player = PlayKitManager.loadPlayer(this, createYouboraPlugin());
+        player = PlayKitManager.loadPlayer(this, pluginConfigs);
 
         //Subscribe to analytics report event.
         subscribeToYouboraReportEvent();
@@ -98,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
         //Prepare player with media config.
         player.prepare(mediaConfig);
 
-        player.play();
     }
 
     /**
@@ -109,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
     private PKPluginConfigs createYouboraPlugin() {
 
         //Important!!! First you need to register your plugin.
-     //   PlayKitManager.registerPlugins(this, YouboraPlugin.factory);
+        PlayKitManager.registerPlugins(this, YouboraPlugin.factory);
 
         //Initialize PKPluginConfigs object.
         PKPluginConfigs pluginConfigs = new PKPluginConfigs();
@@ -172,12 +193,21 @@ public class MainActivity extends AppCompatActivity {
         youboraConfigJson.add("properties", propertiesJson);
         youboraConfigJson.add("extraParams", extraParamJson);
 
-        pluginConfigs.setPluginConfig(YouboraPlugin.factory.getName(), createYouboraPlugin());
+        //Set plugin entry to the plugin configs.
+        pluginConfigs.setPluginConfig(YouboraPlugin.factory.getName(), youboraConfigJson);
 
         return pluginConfigs;
     }
 
-    private Bundle getYouboraBundle() {
+    private PKPluginConfigs getYouboraBundle() {
+
+        //Important!!! First you need to register your plugin.
+        PlayKitManager.registerPlugins(this, YouboraPlugin.factory);
+
+        //Initialize PKPluginConfigs object.
+        PKPluginConfigs pluginConfigs = new PKPluginConfigs();
+        //Initialize Json object that will hold all the configurations for the plugin.
+
 
         Bundle optBundle = new Bundle();
 
@@ -206,22 +236,32 @@ public class MainActivity extends AppCompatActivity {
         optBundle.putString(KEY_CONTENT_GENRE, GENRE);
         optBundle.putString(KEY_CONTENT_TYPE, TYPE);
         optBundle.putString(KEY_CONTENT_TRANSACTION_CODE, TRANSACTION_TYPE); // NEED TO CHECK
-        optBundle.putString(KEY_CONTENT_METADATA, YEAR);
-        optBundle.putString(KEY_CONTENT_METADATA, CAST);
-        optBundle.putString(KEY_CONTENT_METADATA, DIRECTOR);
-        optBundle.putString(KEY_CONTENT_METADATA, OWNER);
-        optBundle.putString(KEY_CONTENT_METADATA, PARENTAL);
+
+        // Create Content Metadata bundle
+        Bundle contentMetaDataBundle = new Bundle();
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_YEAR, YEAR);
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_CAST, CAST);
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_DIRECTOR, DIRECTOR);
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_OWNER, OWNER);
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_PARENTAL, PARENTAL);
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_RATING, RATING);
+        contentMetaDataBundle.putString(KEY_CONTENT_METADATA_QUALITY, QUALITY);
+
+        // Add Content Metadata bundle to the main Options bundle
+        optBundle.putBundle(KEY_CONTENT_METADATA, contentMetaDataBundle);
+
         optBundle.putString(KEY_CONTENT_PRICE, PRICE);
-        optBundle.putString(KEY_CONTENT_METADATA, RATING);
         optBundle.putString(KEY_CONTENT_ENCODING_AUDIO_CODEC, AUDIO_TYPE); // NEED TO CHECK
         optBundle.putString(KEY_CONTENT_CHANNEL, AUDIO_CHANNELS);  // NEED TO CHECK
-        optBundle.putString(KEY_CONTENT_METADATA, QUALITY);
 
         //You can add some extra params here:
         optBundle.putString(KEY_CUSTOM_DIMENSION_1, EXTRA_PARAM_1);
         optBundle.putString(KEY_CUSTOM_DIMENSION_2, EXTRA_PARAM_2);
 
-        return optBundle;
+        //Set plugin entry to the plugin configs.
+        pluginConfigs.setPluginConfig(YouboraPlugin.factory.getName(), optBundle);
+
+        return pluginConfigs;
     }
 
     /**
@@ -303,7 +343,7 @@ public class MainActivity extends AppCompatActivity {
         mediaSource.setUrl(SOURCE_URL);
 
         //Set the format of the source. In our case it will be hls.
-        mediaSource.setMediaFormat(PKMediaFormat.hls);
+        mediaSource.setMediaFormat(PKMediaFormat.dash);
 
         //Add media source to the list.
         mediaSources.add(mediaSource);

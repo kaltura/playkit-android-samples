41da0e5e845d2a10541779da80e95fafbcf98e93
Gilad Nadav <gilad.nadav@kaltura.com>
2019-10-07 11:31:01 +0300

    fix compilation

--- MediaSession/app/src/main/java/com/kaltura/playkit/samples/mediasession/MainActivity.java
+++ MediaSession/app/src/main/java/com/kaltura/playkit/samples/mediasession/MainActivity.java
@@ -15,7 +15,6 @@ import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
-
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;

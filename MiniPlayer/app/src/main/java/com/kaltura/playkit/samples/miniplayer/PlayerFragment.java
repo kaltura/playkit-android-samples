08d78b9d0b3d5c4aefbacf2dac76ccf7452791e8
Gourav Saxena <gourav.saxena@kaltura.com>
2020-04-10 13:49:03 +0530

    - Updated source url

--- MiniPlayer/app/src/main/java/com/kaltura/playkit/samples/miniplayer/PlayerFragment.java
+++ MiniPlayer/app/src/main/java/com/kaltura/playkit/samples/miniplayer/PlayerFragment.java
@@ -70,7 +70,7 @@ public class PlayerFragment extends Fragment {
         PKMediaEntry entry = new PKMediaEntry();
         PKMediaSource source = new PKMediaSource();
         source.setId("0_uka1msg4");
-        source.setUrl("http://api-preprod.ott.kaltura.com/v4_2/api_v3/service/assetFile/action/playManifest/partnerId/198/assetId/259295/assetType/media/assetFileId/516109/contextType/PLAYBACK/a.m3u8");
+        source.setUrl("https://cdnapisec.kaltura.com/p/2215841/sp/221584100/playManifest/entryId/1_w9zx2eti/protocol/https/format/applehttp/falvorIds/1_1obpcggb,1_yyuvftfz,1_1xdbzoa6,1_k16ccgto,1_djdf6bk8/a.m3u8");
         source.setMediaFormat(PKMediaFormat.hls);
         entry.setId("0_uka1msg4");
         entry.setDuration(102000);

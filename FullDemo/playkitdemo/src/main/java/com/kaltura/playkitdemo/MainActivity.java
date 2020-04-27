807f801a150e27d9e1cdeb3a9971578ae0002cc1
Gilad Nadav <gilad.nadav@kaltura.com>
2020-04-27 12:22:21 +0300

    4.7.0

--- FullDemo/playkitdemo/src/main/java/com/kaltura/playkitdemo/MainActivity.java
+++ FullDemo/playkitdemo/src/main/java/com/kaltura/playkitdemo/MainActivity.java
@@ -49,7 +49,7 @@ import com.kaltura.playkit.PKMediaEntry;
 import com.kaltura.playkit.PKMediaSource;
 import com.kaltura.playkit.PKPluginConfigs;
 import com.kaltura.playkit.PKRequestParams;
-import com.kaltura.playkit.PKVideoCodec;
+//import com.kaltura.playkit.PKVideoCodec;
 import com.kaltura.playkit.PlayKitManager;
 import com.kaltura.playkit.Player;
 import com.kaltura.playkit.PlayerEvent;

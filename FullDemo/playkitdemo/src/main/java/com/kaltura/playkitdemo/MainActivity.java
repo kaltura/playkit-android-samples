b072223e30690b6a68e0ba19470764716903c12f
Gourav Saxena <gourav.saxena@kaltura.com>
2020-07-23 15:42:02 +0530

    - Replaced faulty IMADAI cmsid to the working one

--- FullDemo/playkitdemo/src/main/java/com/kaltura/playkitdemo/MainActivity.java
+++ FullDemo/playkitdemo/src/main/java/com/kaltura/playkitdemo/MainActivity.java
@@ -878,7 +878,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private IMADAIConfig getDAIConfig1() {
         String assetTitle = "VOD - Tears of Steel";
         String apiKey = null;
-        String contentSourceId = "2477953";
+        String contentSourceId = "19463";
         String videoId = "tears-of-steel";
         StreamRequest.StreamFormat streamFormat = StreamRequest.StreamFormat.HLS;
         String licenseUrl = null;
